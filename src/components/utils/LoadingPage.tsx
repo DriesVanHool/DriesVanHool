059cea7650deae8c924684590becff7329b73c8a
r0895226 <46358235+DriesVanHool@users.noreply.github.com>
2023-12-24 12:14:19 +0100

    Loading page

--- src/components/utils/LoadingPage.tsx
+++ src/components/utils/LoadingPage.tsx
@@ -1,37 +1,21 @@
-import styled from 'styled-components'
+import {FunctionComponent, useContext} from 'react'
+import {Progress} from '@nextui-org/react'
+import {ThemeContext} from '../providers/ThemeProvider.tsx'
 
-const LoadingPage = styled.span`
-  width: 150px; /* control the size */
-  aspect-ratio: 1;
-  display: grid;
-  mask: conic-gradient(from 22deg, #0003, #000);
-  animation: load 1s steps(8) infinite;
-
-  position: fixed;
-  z-index: 999999;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto;
-  flex-grow: 1;
-
-  &,
-  &:before {
-    --_g: linear-gradient(#17177c 0 0) 50%; /* update the color here */
-    background:
-              var(--_g)/34% 8%  space no-repeat,
-              var(--_g)/8%  34% no-repeat space;
-  }
-
-  &:before {
-    content: "";
-    transform: rotate(45deg);
-  }
-
-  @keyframes load {
-    to { transform: rotate(1turn); }
-  }
-`
+const LoadingPage: FunctionComponent = () => {
+    const {theme} = useContext(ThemeContext)
+    return (
+        <div className={`${theme} text-foreground bg-background relative flex flex-col h-100 min-h-screen justify-center items-center`}>
+            <Progress
+                size="lg"
+                isIndeterminate
+                color="warning"
+                aria-label="Loading..."
+                className="max-w-md"
+            />
+            <p className="text-center mt-3">Loading...</p>
+        </div>
+    );
+}
 
 export default LoadingPage
