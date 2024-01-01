b117980662c55d1fc5fd66ec216a6908b1825ffa
r0895226 <46358235+DriesVanHool@users.noreply.github.com>
2024-01-01 21:47:22 +0100

    Vite config for vercel styling issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   css: {
-    postcss: {},
+    postcss: {
+      plugins: [
+        require('autoprefixer'),
+        require('tailwindcss')
+      ],
+    },
   },
 })
